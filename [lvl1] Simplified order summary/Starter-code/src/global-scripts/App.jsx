8ea78c4b88d8b9ee21c3fbb3f31194c674725783
Patryk Mormul <104906717+PatrykMO@users.noreply.github.com>
2023-05-09 18:06:51 +0000

    Responsive designe setup

---
+++ [lvl1] Simplified order summary/Starter-code/src/global-scripts/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Summary from './scripts/Components/Summary/Summary'
+
+import '../global-styles/App.scss';
+
+const App = () => {
+  return (
+    <div className='app'>
+        <Summary />
+    </div>
+  )
+}
+
+export default App
---
+++ [lvl1] Simplified order summary/Starter-code/src/global-scripts/scripts/Components/Summary/Summary.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import './Summary.scss';
+
+const Summary = () => {
+  return (
+    <div className='container'>
+
+        <div className="img-container">
+            <img
+                src='assets/hero.jpg'
+            />
+        </div>
+
+        <div className="details">
+
+            <div className="summary">
+                <h1>Ordery Summary</h1>
+                <p>You're close! Confirm your purchase and complete your order in the next step</p>
+            </div>
+
+            <div className="plan-container">
+
+                <div className="left">
+                    <img
+                        src='/assets/icons/90.png'
+                    />
+                    <div className='element'/>
+                    <div className="plan-type">
+                        <h2>Annual Plan</h2>
+                        <p>$58.99 / year</p>
+                    </div>
+                </div>
+
+                <div className="right">
+                   <h2>Change</h2>
+                </div>
+
+            </div>
+
+            <div className="order-options">
+
+                <button>Proceed to Payment</button>
+                <h2>Cancel Order</h2>
+
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Summary
