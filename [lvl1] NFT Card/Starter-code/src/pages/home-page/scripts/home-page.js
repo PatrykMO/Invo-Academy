4817d1b13d666fdf41aa1d5fcb93003ba0ceae0a
Patryk Mormul <104906717+PatrykMO@users.noreply.github.com>
2023-05-23 12:10:50 +0000

    BEM fix

--- [lvl1] NFT Card/Starter-code/src/pages/home-page/scripts/home-page.js
+++ [lvl1] NFT Card/Starter-code/src/pages/home-page/scripts/home-page.js
@@ -3,54 +3,54 @@
 import '../styles/home-page.scss';
 
 const html = `
-    <div class="nftcard-component">
+    <div class="nftcard">
 
-        <div class="nftcard-component__background">
+        <div class="nftcard__background">
             <img
                 src='/static/images/Hero.png'
-                class="nftcard-component__background__img"
+                class="nftcard__background--img"
             />
         </div>
 
-        <div class="nftcard-component__content">
-            <div class="nftcard-component__content__info-container">
-                <h1 class='nftcard-component__content__info-container__title'>
+        <div class="nftcard__content">
+            <div class="nftcard__content--info-container">
+                <h1 class='nftcard__content--title'>
                     Equilibrium #3429
                 </h1>
-                <h3 class='nftcard-component__content__info-container__desc'>
+                <h3 class='nftcard__content--desc'>
                     Equilibrium is an NFT that will appeal to many players.
                 </h3>
             </div>
 
-            <div class="nftcard-component__content__details-container">
-                <div class="nftcard-component__content__details-container__price">
+            <div class="nftcard__content--details-container">
+                <div class="nftcard__content--price-container">
                     <img
                         src='/static/images/diamond.png'
                         alt='diamond'
-                        class="nftcard-component__content__details-container__price__icon"
+                        class="nftcard__content--icon"
                     />
-                    <h3 class="nftcard-component__content__details-container__price__value">0.041 ETH</h3>
+                    <h3 class="nftcard__content--value">0.041 ETH</h3>
                 </div>
 
-                <div class="nftcard-component__content__details-container__time">
+                <div class="nftcard__content--time-container">
                     <img
                         src='/static/images/clock.png'
                         alt='clock'
-                        class="nftcard-component__content__details-container__time__icon"
+                        class="nftcard__content--icon"
                     />
-                    <p class="nftcard-component__content__details-container__time__value">3 days left</p>
+                    <p class="nftcard__content--time-value">3 days left</p>
                 </div>
             </div>
 
-            <div class="nftcard-component__content__stroke"></div>
+            <div class="nftcard__content--stroke"></div>
 
-            <div class="nftcard-component__content__author">
+            <div class="nftcard__content--author">
                 <img
                     src='/static/images/Eva.jpg'
                     alt='Eva'
-                    class="nftcard-component__content__author__icon"
+                    class="nftcard__content--photo"
                 />
-                <h3 class="nftcard-component__content__author__name">
+                <h3 class="nftcard__content--name">
                     Creation of <span>Eva Smitch</span>
                 </h3>
             </div>
