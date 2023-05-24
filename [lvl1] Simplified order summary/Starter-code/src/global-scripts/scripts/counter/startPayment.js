4cee889f84df683064fd1e6eec94a72586b60afa
Patryk Mormul <104906717+PatrykMO@users.noreply.github.com>
2023-05-24 18:22:45 +0000

    Fix animations

--- [lvl1] Simplified order summary/Starter-code/src/global-scripts/scripts/counter/startPayment.js
+++ [lvl1] Simplified order summary/Starter-code/src/global-scripts/scripts/counter/startPayment.js
@@ -1,32 +1,23 @@
 /// Sample js file for demo purposes
 /// Remember to always export your functions
 /// so that they can be used in other files
-
 export function startPayment() {
-  const containerElement = document.querySelector('.component-container');
+  const paymentButton = document.getElementById("paymentButton");
+  const buttonText = document.getElementById("buttonText");
+  const loadingSpinner = document.getElementById("loadingSpinner");
 
-  const loaderContainer = document.createElement("div");
-  loaderContainer.classList.add("loader-container");
-
-  var paymentButton = document.getElementById("paymentButton");
-  var loader = document.createElement("div");
-  loader.id = "loader";
-
-  const loaderElement = document.createElement("div");
-  loaderElement.classList.add("loader");
-
-  loaderContainer.appendChild(loaderElement);
-  containerElement.insertAdjacentElement("afterend", loaderContainer);
-
-  containerElement.style.visibility = 'hidden';
+  paymentButton.disabled = true;
+  buttonText.style.display = "none";
+  loadingSpinner.style.display = "inline";
 
   setTimeout(function() {
-    loaderContainer.remove();
-    containerElement.style.visibility = 'visible';
-    paymentButton.innerHTML = "Payment successful!";
+    buttonText.style.display = "inline";
+    loadingSpinner.style.display = "none";
+    buttonText.innerHTML = "Payment successful!";
   }, 3000);
 
   setTimeout(function() {
-    paymentButton.innerHTML = "Proceed to Payment";
+    paymentButton.disabled = false;
+    buttonText.innerHTML = "Proceed to Payment";
   }, 6000);
 }
--- [lvl1] Simplified order summary/Starter-code/src/pages/home-page/scripts/home-page.js
+++ [lvl1] Simplified order summary/Starter-code/src/pages/home-page/scripts/home-page.js
@@ -6,67 +6,69 @@ import { startPayment } from '../../../global-scripts/main';
 
 
 const html = `
-<div class="component-container" id="loading">
-    <div class="image-container">
-    <img
-        src="/static/images/women.png"
-        alt="Women listening to music"
-        class="image-container__img"/>
-    </div>
-
-    <div class="content-container">
-    <div class="content-container__info">
-        <h1 class="content-container__info__title">
-        Order Summary
-        </h1>
-        <p class="content-container__info__desc">
-        You're close! Confirm your purchase and complete your order in the next step
-        </p>
-    </div>
-
-    <div class="content-container__plan">
-        <div class="content-container__plan__details">
+    <div class="component-container">
+        <div class="image-container">
             <img
-                src="/static/images/music.png"
-                alt=""
-                class="content-container__plan__details__icon"
-            >
-
-            <div class="content-container__plan__details__stroke"></div>
-
-            <div class="content-container__plan__details__info">
-                <h2 class="content-container__plan__details__info__type">
-                Annual Plan
-                </h2>
-                <p class="content-container__plan__details__info__price">
-                $58.99 / year
-                </p>
-            </div>
+                src="/static/images/women.png"
+                alt="Women listening to music"
+                class="image-container__content"
+            />
         </div>
 
-        <a href="#">
-            <h3 class="content-container__plan__mod">
-            Change
-            </h3>
-        </a>
-    </div>
+        <div class="content-container">
+            <div class="content-container__info-container">
+                <h1 class="content-container__info-title">
+                    Order Summary
+                </h1>
+                <p class="content-container__info-desc">
+                    You're close! Confirm your purchase and complete your order in the next step
+                </p>
+            </div>
 
-    <div class="payment-container">
-        <button
-        class="button"
-        id="paymentButton"
-        >
-        Proceed to Payment
-        </button>
+            <div class="content-container__plan-container">
+                <div class="content-container__plan-details">
+                    <img
+                        src="/static/images/music.png"
+                        alt=""
+                        class="content-container__plan-icon"
+                    >
 
-        <a href="#">
-            <h3 class="payment-container__cancelation">
-                Cancel Order
-            </h3>
-        </a>
+                    <div class="content-container__plan-stroke"></div>
+
+                    <div class="content-container__plan-info">
+                        <h2 class="content-container__plan-type">
+                            Annual Plan
+                        </h2>
+                        <p class="content-container__plan-price">
+                            $58.99 / year
+                        </p>
+                    </div>
+                </div>
+
+                <a href="#" class="content-container__plan-link">
+                    <h3 class="content-container__plan-mod">
+                    Change
+                    </h3>
+                </a>
+            </div>
+
+            <div class="payment-container">
+                <button
+                    class="payment-container__button"
+                    id="paymentButton"
+                >
+                    <span id="buttonText" class="payment-container__text">Proceed to Payment</span>
+                    <span id="loadingSpinner" class="payment-container__spinner"></span>
+                </button>
+
+                <a href="#" class="payment-container__cancelation-link--nounderline">
+                    <h3 class="payment-container__cancelation">
+                        Cancel Order
+                    </h3>
+                </a>
+            </div>
+        </div>
     </div>
-    </div>
-</div>
 `;
 
 const container = document.getElementById("home-page");
