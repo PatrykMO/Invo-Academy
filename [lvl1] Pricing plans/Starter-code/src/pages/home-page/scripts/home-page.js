4b3490f7431022ac2b191b3eadaa7f159ccb44a5
Patryk Mormul <104906717+PatrykMO@users.noreply.github.com>
2023-05-30 09:06:21 +0000

    Fix based on mentor's feedback

--- [lvl1] Pricing plans/Starter-code/src/pages/home-page/scripts/home-page.js
+++ [lvl1] Pricing plans/Starter-code/src/pages/home-page/scripts/home-page.js
@@ -16,41 +16,41 @@ const html = `
         </header>
 
         <main class="main">
-            <div class="card-container">
-                <div class="card-container__content">
-                    <h1 class="card-container__content-title">
+            <div class="card">
+                <div class="card__content">
+                    <h1 class="card__title">
                         Basic
                     </h1>
 
-                    <div class="card-container__content__price-container">
-                        <span class="card-container__content__price-currency">$</span>
-                        <h1 class="card-container__content__price-value">399</h1>
-                        <span class="card-container__content__price-period">/year</span>
+                    <div class="card__price-container">
+                        <span class="card__price-currency">$</span>
+                        <h1 class="card__price-value">399</h1>
+                        <span class="card__price-period">/year</span>
                     </div>
 
-                    <h2 class="card-container__content-desc">
+                    <h2 class="card__desc">
                         For organizing every corner of your work & life.
                     </h2>
 
-                    <div class="card-container__content__benefits-container">
-                        <div class="card-container__content__benefits-info">
+                    <div class="card__benefits-container">
+                        <div class="card__benefits-info">
                             <img
                                 src="/static/icons/icon.png"
                                 alt="check icon"
-                                class="card-container__content__benefits-img"
+                                class="card__benefits-img"
                             />
-                            <p class="card-container__content__benefits-desc">
+                            <p class="card__benefits-desc">
                                 Collaborative workspace
                             </p>
                         </div>
 
-                        <div class="card-container__content__benefits-info">
+                        <div class="card__benefits-info">
                             <img
                                 src="/static/icons/icon.png"
                                 alt="check icon"
-                                class="card-container__content__benefits-img"
+                                class="card__benefits-img"
                             />
-                            <p class="card-container__content__benefits-desc">
+                            <p class="card__benefits-desc">
                                 7 day page history
                             </p>
                         </div>
@@ -64,122 +64,122 @@ const html = `
                 </div>
             </div>
 
-            <div class="card-container--dark">
-                <div class="card-container__content">
-                    <h1 class="card-container__content-title--dark">
+            <div class="card--dark">
+                <div class="card__content">
+                    <h1 class="card__title--dark">
                         Professional
                     </h1>
 
-                    <div class="card-container__content__price-container--dark">
-                        <span class="card-container__content__price-currency">$</span>
-                        <h1 class="card-container__content__price-value">499</h1>
-                        <span class="card-container__content__price-period">/year</span>
+                    <div class="card__price-container--dark">
+                        <span class="card__price-currency">$</span>
+                        <h1 class="card__price-value">499</h1>
+                        <span class="card__price-period">/year</span>
                     </div>
 
-                    <h2 class="card-container__content-desc">
+                    <h2 class="card__desc">
                         A place for small groups to plan & get organized.
                     </h2>
 
-                    <div class="card-container__content__benefits-container--dark">
-                        <div class="card-container__content__benefits-info">
+                    <div class="card__benefits-container--dark">
+                        <div class="card__benefits-info">
                             <img
                                 src="/static/icons/icon.png"
                                 alt="check icon"
-                                class="card-container__content__benefits-img"
+                                class="card__benefits-img"
                             />
-                            <p class="card-container__content__benefits-desc">
+                            <p class="card__benefits-desc">
                                 All features in Basic
                             </p>
                         </div>
 
-                        <div class="card-container__content__benefits-info">
+                        <div class="card__benefits-info">
                             <img
                                 src="/static/icons/icon.png"
                                 alt="check icon"
-                                class="card-container__content__benefits-img"
+                                class="card__benefits-img"
                             />
-                            <p class="card-container__content__benefits-desc">
+                            <p class="card__benefits-desc">
                                 Unlimited blocks for teams
                             </p>
                         </div>
 
-                        <div class="card-container__content__benefits-info">
+                        <div class="card__benefits-info">
                             <img
                                 src="/static/icons/icon.png"
                                 alt="check icon"
-                                class="card-container__content__benefits-img"
+                                class="card__benefits-img"
                             />
-                            <p class="card-container__content__benefits-desc">
+                            <p class="card__benefits-desc">
                                 30 day page history
                             </p>
                         </div>
                     </div>
 
                     <a href="#">
-                        <button class="button --purple">
+                        <button class="button--purple">
                         Get started
                         </button>
                     </a>
                 </div>
             </div>
 
-            <div class="card-container">
-                <div class="card-container__content">
-                    <h1 class="card-container__content-title">
+            <div class="card">
+                <div class="card__content">
+                    <h1 class="card__title">
                         Enterprise
                     </h1>
 
-                    <div class="card-container__content__price-container">
-                        <span class="card-container__content__price-currency">$</span>
-                        <h1 class="card-container__content__price-value">599</h1>
-                        <span class="card-container__content__price-period">/year</span>
+                    <div class="card__price-container">
+                        <span class="card__price-currency">$</span>
+                        <h1 class="card__price-value">599</h1>
+                        <span class="card__price-period">/year</span>
                     </div>
 
-                    <h2 class="card-container__content-desc">
+                    <h2 class="card__desc">
                         For companies using our software to connect several teams & tools.
                     </h2>
 
-                    <div class="card-container__content__benefits-container">
-                        <div class="card-container__content__benefits-info">
+                    <div class="card__benefits-container">
+                        <div class="card__benefits-info">
                             <img
                                 src="/static/icons/icon.png"
                                 alt="check icon"
-                                class="card-container__content__benefits-img"
+                                class="card__benefits-img"
                             />
-                            <p class="card-container__content__benefits-desc">
+                            <p class="card__benefits-desc">
                                 SAML SSO
                             </p>
                         </div>
 
-                        <div class="card-container__content__benefits-info">
+                        <div class="card__benefits-info">
                             <img
                                 src="/static/icons/icon.png"
                                 alt="check icon"
-                                class="card-container__content__benefits-img"
+                                class="card__benefits-img"
                             />
-                            <p class="card-container__content__benefits-desc">
+                            <p class="card__benefits-desc">
                                 Private teamspaces
                             </p>
                         </div>
 
-                        <div class="card-container__content__benefits-info">
+                        <div class="card__benefits-info">
                             <img
                                 src="/static/icons/icon.png"
                                 alt="check icon"
-                                class="card-container__content__benefits-img"
+                                class="card__benefits-img"
                             />
-                            <p class="card-container__content__benefits-desc">
+                            <p class="card__benefits-desc">
                                 Advanced page analytics
                             </p>
                         </div>
 
-                        <div class="card-container__content__benefits-info">
+                        <div class="card__benefits-info">
                             <img
                                 src="/static/icons/icon.png"
                                 alt="check icon"
-                                class="card-container__content__benefits-img"
+                                class="card__benefits-img"
                             />
-                            <p class="card-container__content__benefits-desc">
+                            <p class="card__benefits-desc">
                                 90 day page history
                             </p>
                         </div>
@@ -192,8 +192,6 @@ const html = `
                     </a>
                 </div>
             </div>
-
-            
         </main>
     </div>
 `;
